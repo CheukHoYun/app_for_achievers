8e4213ee7e94978f58867bba6241d1eb5ef3e4c3
Cheuk Ho Yun <cheukho.yun@mail.utoronto.ca>
2020-08-18 14:49:59 -0400

    Updates

---
+++ precache-manifest.c90798e9cfa71d84cc256765fb66a271.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d50c0d160c16990c38f761150ec02a6",
+    "url": "/app_for_achievers/index.html"
+  },
+  {
+    "revision": "fc2d524d79bf0429bd65",
+    "url": "/app_for_achievers/static/js/2.004ff0f2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/app_for_achievers/static/js/2.004ff0f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "959ab12fe84b52a52bd4",
+    "url": "/app_for_achievers/static/js/main.b4652146.chunk.js"
+  },
+  {
+    "revision": "b15ee9afe558d7e9b674",
+    "url": "/app_for_achievers/static/js/runtime-main.6f1a65d1.js"
+  }
+]);
