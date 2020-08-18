36c283deece5c91f59cd39f7e9b69367b33d5c29
Cheuk Ho Yun <cheukho.yun@mail.utoronto.ca>
2020-08-18 15:20:26 -0400

    Making sure everything works

---
+++ src/components/Content.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+
+class Content extends Component {
+  state = {};
+  render() {
+    return <main>This is the content!</main>;
+  }
+}
+
+export default Content;
