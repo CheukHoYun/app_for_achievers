0e589e395ec06c0bc4d9d87e201d11bc5c599613
Cheuk Ho Yun <cheukho.yun@mail.utoronto.ca>
2020-08-18 23:52:11 -0400

    Updates

---
+++ precache-manifest.2dea6864f07c9e5e72a02d53dccf1218.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0e66ee569c94da2bd924e74de2a6a22",
+    "url": "/app_for_achievers/index.html"
+  },
+  {
+    "revision": "d9b6b2596786f8921492",
+    "url": "/app_for_achievers/static/css/2.f3cffc9e.chunk.css"
+  },
+  {
+    "revision": "06032a4ce842fcae0e6e",
+    "url": "/app_for_achievers/static/css/main.090e479f.chunk.css"
+  },
+  {
+    "revision": "d9b6b2596786f8921492",
+    "url": "/app_for_achievers/static/js/2.66f6e6f1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/app_for_achievers/static/js/2.66f6e6f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "06032a4ce842fcae0e6e",
+    "url": "/app_for_achievers/static/js/main.c1a289d1.chunk.js"
+  },
+  {
+    "revision": "b15ee9afe558d7e9b674",
+    "url": "/app_for_achievers/static/js/runtime-main.6f1a65d1.js"
+  }
+]);
